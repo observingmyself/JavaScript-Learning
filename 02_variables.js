5205c7434f090148476433fc33889e70f4c7d7f6
Pardeep Singh <164214223+observingmyself@users.noreply.github.com>
2024-07-20 06:54:42 +0000

    first commit

---
+++ 02_variables.js
@@ -0,0 +1,20 @@
+var a = 1;
+// it can be updated and redeclared if needed 
+var a = 2;
+a = 3 
+// in the upper case value of a is 3 because i have updated it in the end 
+console.log(a)
+
+// let 
+let b = 1 
+// this can be updated but not declared again
+b = 2 
+// let b = 2 // we can do this 
+console.log(b)
+
+const c = 1 ;
+// this can not be updated or not declared again
+// const c = 2 // you can't do this
+// c = 3 ; 
+// you also can't do the above
+console.log(c)
---
+++ 03_datatypes.js
@@ -0,0 +1,23 @@
+"use strict"; // now this will use newer version of js
+// alert(3) we can't use this statement because we are using nodejs not browser
+
+let name = "Pardeep" // string type
+let age = 20 // int type 
+let isLoggedIn = true // bool type 
+let state ; 
+console.log(typeof(state))
+
+// primitive data types
+
+// numbers 
+// numbers varies on the length of our  number
+// string it is defined in the double quotes 
+// name = "gorav"
+// boolean type like true and false
+// null => standalone value
+// undefined a value which is not defined
+// symbol => unique
+
+// non primitive data types
+
+
