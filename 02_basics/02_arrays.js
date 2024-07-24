37ff8e2ba190912a5ade93dde9b14bef2305a3bf
Pardeep Singh <164214223+observingmyself@users.noreply.github.com>
2024-07-24 18:21:19 +0000

    arrays

---
+++ 02_basics/02_arrays.js
@@ -0,0 +1,14 @@
+let marvel_heros = ["Ironman","Hulk","Spiderman","HawkEye"]
+let dc_heros = ["Superman","Batman","Aquaman","Flash"]
+
+// marvel_heros.push(dc_heros)
+// console.log(marvel_heros)
+const allHeros = marvel_heros.concat(dc_heros)
+// console.log(allHeros)
+const name = "pardeep"
+console.log(Array.from(name))
+
+const score1 = 100
+const score2 = 200 
+const score3 = 300
+console.log(Array.of(score1,score2,score3))
