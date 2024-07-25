1129ea349002f02a38924aa3be503f7986911e3f
Pardeep Singh <164214223+observingmyself@users.noreply.github.com>
2024-07-25 05:26:21 +0000

    second comit

--- 03_basics/02_scope.js
+++ 03_basics/02_scope.js
@@ -17,3 +17,49 @@ const c = 300
 console.log(a)
 console.log(b)
 console.log(c)
+
+
+function one(){
+    const username = "Pardeep"
+
+    function two(){
+        const website = "Linkedin"
+        console.log(username);
+    }
+    // console.log(website);
+
+     two()
+
+}
+
+// one()
+
+if (true) {
+    const username = "Pardeep"
+    if (username === "Pardeep") {
+        const website = " Link.in"
+        // console.log(username + website);
+    }
+    // console.log(website);
+}
+
+// console.log(username);
+
+
+// ++++++++++++++++++ interesting ++++++++++++++++++
+
+
+console.log(addone(5))
+
+function addone(num){
+    return num + 1
+}
+
+
+
+const addTwo = function(num){
+    return num + 2
+}
+addTwo(5)
+
+
