758508385996559e32904e9cafcbe94beacdb9b9
Pardeep Singh <164214223+observingmyself@users.noreply.github.com>
2024-07-29 08:42:11 +0000

    color changing project

---
+++ 07_projects/Unlimited Colors /script.js
@@ -0,0 +1,32 @@
+let generatedColor = function(){
+    let code = '#'
+    let hex = '1234567890ABCDEF'
+    for(let i=0; i<6; i++){
+        code += hex[Math.floor(Math.random()*16)]
+    }
+    return code 
+}
+
+function girgit(){
+    
+    document.body.style.backgroundColor = generatedColor()
+}
+let intervalId; 
+document.querySelector('#start').addEventListener('click',function(){
+    if(!intervalId){
+        intervalId = setInterval(girgit,1000)
+    }
+     
+})
+
+document.querySelector('#stop').addEventListener('click',function(){
+     clearInterval(intervalId)
+     intervalId = null
+})
+
+
+
+
+
+
+
